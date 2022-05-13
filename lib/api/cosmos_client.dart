23c5687fa16610e25ee833168942ee21a398838b
Prad Nukala <pradn@sonr.io>
2022-05-12 20:24:07 -0400

    :art: Add Boilerplate code

---
+++ lib/api/cosmos_client.dart
@@ -0,0 +1,59 @@
+import 'package:sacco/sacco.dart';
+
+void walletTest() async {
+  // -----------------------------------
+  // --- Creating a wallet
+  // -----------------------------------
+
+  final networkInfo = NetworkInfo(
+    bech32Hrp: 'did:com:',
+    lcdUrl: Uri.parse('http://localhost:1337'),
+  );
+
+  const mnemonicString =
+      'vivid favorite regular curve check word bubble echo disorder cute parade neck rib evidence option glimpse couple force angry section dizzy puppy express cream';
+  final mnemonic = mnemonicString.split(' ');
+  final wallet = Wallet.derive(mnemonic, networkInfo);
+
+  // -----------------------------------
+  // --- Creating a transaction
+  // -----------------------------------
+
+  final message = StdMsg(
+    type: 'cosmos-sdk/MsgSend',
+    value: {
+      'from_address': wallet.bech32Address,
+      'to_address': 'did:com:1lys5uu683wrmupn4zguz7f2gqw45qae98pzn3d',
+      'amount': const [
+        {
+          'denom': 'uatom',
+          'amount': '100',
+        }
+      ]
+    },
+  );
+
+  final stdTx = TxBuilder.buildStdTx(stdMsgs: [message]);
+
+  // -----------------------------------
+  // Signing a transaction
+  // -----------------------------------
+
+  final signedStdTx = await TxSigner.signStdTx(wallet: wallet, stdTx: stdTx);
+
+  // -----------------------------------
+  // --- Sending a transaction
+  // -----------------------------------
+
+  final result = await TxSender.broadcastStdTx(
+    wallet: wallet,
+    stdTx: signedStdTx,
+  );
+
+  // Check the result
+  if (result.success) {
+    print('Tx send successfully. Hash: ${result.hash}');
+  } else {
+    print('Tx send error: ${result.error?.errorMessage}');
+  }
+}
